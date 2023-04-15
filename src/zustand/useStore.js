cd583780ef9d5b672305d68d3781ae4c44134c6c
Augustin Anders <augustinanders@gmail.com>
2023-04-15 12:26:55 +0200

    add combined useStore

---
+++ src/zustand/useStore.js
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+
+const useStore = create((set) => {
+  return {
+    numFailedAttempts: 0,
+    addOneFailedAttempt: () =>
+      set((state) => ({ numFailedAttempts: state.numFailedAttempts + 1 })),
+    resetFailedAttempts: () => set(() => ({ numFailedAttempts: 0 })),
+
+    time: 0,
+    formattedTime: "00:00",
+    timerOn: false,
+    addOneSecond: () =>
+      set((state) => ({
+        time: state.time + 1,
+        formattedTime: formatTime(state.time + 1),
+      })),
+    startTimer: () => set(() => ({ timerOn: true })),
+    stopTimer: () => set(() => ({ timerOn: false })),
+    resetTimer: () => set(() => ({ time: 0, formattedTime: "00:00" })),
+
+    isVictory: false,
+    setIsVictory: () => set(() => ({ isVictory: true })),
+    closeIsVictory: () => set(() => ({ isVictory: false })),
+  };
+});
+
+const formatTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  return `${minutes < 10 ? "0" + minutes : minutes}:${
+    seconds < 10 ? "0" + seconds : seconds
+  }`;
+};
+
+export default useStore;
